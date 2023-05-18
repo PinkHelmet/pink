49d1932101e6d9553d6d80c22e937238160648bd
Arek <83660682+arkadiuszrypulak@users.noreply.github.com>
2023-05-18 21:51:06 +0200

    poprawa kontaktu

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -18,24 +18,32 @@ export default function Contact() {
         transition={{ duration: 0.75, delay: 0.4 }}
         className="container mx-auto h-[70vh]"
       >
-        <div className="flex flex-col w-full h-full justify-center items-center text-2xl">
-          <h1 className="text-4xl mb-4">Masz pytanie? </h1>
-          <p>Chcesz skorzystać z oferty ? </p>
-          <p>Zapraszam do kontaktu:</p>
-          <p>
-            tel:{" "}
-            <span className="text-pink-rose">{siteMetadata.phoneNumber}</span>
-          </p>
-          <p>
-            email: <span className="text-pink-rose">{siteMetadata.email}</span>
-          </p>
-          <h2 className="text-4xl mt-4">Dane firmy:</h2>
-          <p>
-            NIP: <span>{siteMetadata.NIP}</span>
-          </p>
-          <p>
-            REGON: <span>{siteMetadata.REGON}</span>
-          </p>
+        <div className="flex flex-col md:flex-row w-full h-full justify-center items-center text-2xl">
+          <div className="flex flex-col items-center m-6">
+            <h1 className="text-4xl mb-4">Masz pytanie? </h1>
+            <p>Chcesz skorzystać z oferty ? </p>
+            <p>Zapraszam do kontaktu:</p>
+            <p>
+              tel:{" "}
+              <span className="text-pink-rose">{siteMetadata.phoneNumber}</span>
+            </p>
+            <p>
+              email:{" "}
+              <span className="text-pink-rose ">{siteMetadata.email}</span>
+            </p>
+          </div>
+          <div className="flex flex-col items-center m-6">
+            <h2 className="text-4xl mt-4">Dane firmy:</h2>
+            <p className="text-pink-rose font-bold">
+              {siteMetadata.companyName}
+            </p>
+            <p>
+              NIP: <span>{siteMetadata.NIP}</span>
+            </p>
+            <p>
+              REGON: <span>{siteMetadata.REGON}</span>
+            </p>
+          </div>
         </div>
       </motion.section>
     </>
