4671105a6ed9e97792dcf9141d6c337a78ccb875
PinkHelmet <168078065+PinkHelmet@users.noreply.github.com>
2026-05-14 18:19:01 +0200

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -1,29 +1,35 @@
 import Head from "next/head";
 import React from "react";
+import { useRouter } from "next/router";
 
 const HeadSeo = ({
   title,
   description,
-  canonicalUrl,
   ogTwitterImage,
   ogType,
   children,
 }) => {
+  const router = useRouter();
+  const domain = "https://pinkhelmet.pl";
+  
+  // Tworzy pełny adres URL, usuwa zbędne pytajniki i parametry
+  const canonicalUrl = `${domain}${router.asPath === '/' ? '' : router.asPath.split('?')[0]}`;
+
   return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="UFT-8" />
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        {description && <meta name="description" content={description} />}
-        <link rel="canonical" href={canonicalUrl} />{" "}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        {children}
-      </Head>
-    </div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+      {description && <meta name="description" content={description} />}
+      
+      <link rel="canonical" href={canonicalUrl} />
+      
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      {children}
+    </Head>
   );
 };
 
