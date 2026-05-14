34905642de3b712bd5d311cb4a7a79cec885cea8
PinkHelmet <168078065+PinkHelmet@users.noreply.github.com>
2026-05-14 18:25:35 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,27 @@
 import Head from "next/head";
 import React from "react";
+import { useRouter } from "next/router";
 
 const Header = (props) => {
+  const router = useRouter();
+  const domain = "https://pinkhelmet.pl";
+  
+  // Automatyczne generowanie pełnego adresu URL dla Google
+  const canonicalUrl = `${domain}${router.asPath === '/' ? '' : router.asPath.split('?')[0]}`;
+
   return (
-    <div>
+    <>
       <Head>
-        <title>{props.pageTitle}</title>
-        <meta charSet="UFT-8" />
+        <title>{props.pageTitle || "Pink Helmet | Odbiory mieszkań i domów"}</title>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        
+        {/* To jest kluczowa poprawka dla Google */}
+        <link rel="canonical" href={canonicalUrl} />
       </Head>
       <div>{props.children}</div>
-    </div>
+    </>
   );
 };
 
